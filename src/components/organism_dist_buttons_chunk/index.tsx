83c63c7c1fc4e609eeab1c429c9c6f897756724f
AJ Kim <jkim67cloud@gmail.com>
2025-03-29 13:34:20 +0900

    feat

--- src/components/organism_dist_buttons_chunk/index.tsx
+++ src/components/organism_dist_buttons_chunk/index.tsx
@@ -4,6 +4,46 @@ import { Box, Stack, Typography } from "@mui/material";
 import { FC, Fragment, useCallback, useEffect, useState } from "react";
 import NumberPad from "../number-pad";
 
+interface NumberValue {
+  value: number
+  from: number
+  until: number
+}
+
+interface Props {
+  swings: number;
+  teeHeight: number;
+  swingType: number;
+  ballAim: "center" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  stability: number;
+  carry: number;
+  distance: number;
+  ballSpeed: number;
+  launchAngle: number;
+  apex: number;
+  hangTime: number;
+  landingAngle: number;
+  curve: number;
+  offCenter: number;
+}
+
+const myDefault: Props = {
+  swings: 0,
+  teeHeight: 0,
+  swingType: 0,
+  ballAim: "center",
+  stability: 0,
+  carry: 0,
+  distance: 0,
+  ballSpeed: 0,
+  launchAngle: 0,
+  apex: 0,
+  hangTime: 0,
+  landingAngle: 0,
+  curve: 0,
+  offCenter: 0,
+}
+
 const findClosestKeys = (map: Map<number, ISwing>, input: number): ISwing[] => {
   const sortedKeys = Array.from(map.keys()).sort((a, b) => a - b);
 
